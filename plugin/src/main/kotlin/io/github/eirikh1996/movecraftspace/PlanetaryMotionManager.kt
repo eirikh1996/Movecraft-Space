1fa049187c4463e37c59d5ae3a06cff2c836e826
eirikh1996 <eirikhetland1996@gmail.com>
2020-09-12 10:32:20 +0200

    Upon planetary motion, crafts within the planet's destination area will be teleported to the planet moving

--- plugin/src/main/kotlin/io/github/eirikh1996/movecraftspace/PlanetaryMotionManager.kt
+++ plugin/src/main/kotlin/io/github/eirikh1996/movecraftspace/PlanetaryMotionManager.kt
@@ -1,10 +1,19 @@
 package io.github.eirikh1996.movecraftspace
 
+import io.github.eirikh1996.movecraftspace.expansion.ExpansionManager
+import io.github.eirikh1996.movecraftspace.listener.MovecraftListener
 import io.github.eirikh1996.movecraftspace.objects.ImmutableVector
 import io.github.eirikh1996.movecraftspace.objects.Planet
 import io.github.eirikh1996.movecraftspace.objects.PlanetCollection
+import net.countercraft.movecraft.MovecraftChunk
+import net.countercraft.movecraft.MovecraftLocation
+import net.countercraft.movecraft.craft.ChunkManager
+import net.countercraft.movecraft.craft.Craft
+import net.countercraft.movecraft.craft.CraftManager
 import net.countercraft.movecraft.mapUpdater.MapUpdateManager
 import net.countercraft.movecraft.mapUpdater.update.UpdateCommand
+import net.countercraft.movecraft.utils.MathUtils
+import org.bukkit.Bukkit
 import org.bukkit.World
 import org.bukkit.scheduler.BukkitRunnable
 import java.util.*
@@ -14,6 +23,7 @@ import kotlin.math.PI
 import kotlin.math.cos
 import kotlin.math.round
 import kotlin.math.sin
+import kotlin.random.Random
 
 object PlanetaryMotionManager : BukkitRunnable() {
     val updates = LinkedList<PlanetMoveUpdateCommand>()
@@ -77,10 +87,87 @@ object PlanetaryMotionManager : BukkitRunnable() {
         if (moonUpdates.containsKey(poll.planet)) {
             val moonUpdateSet = moonUpdates[poll.planet]!!
             moonUpdateSet.add(poll)
+            for (update in moonUpdateSet) {
+                if (update !is PlanetMoveUpdateCommand)
+                    continue
+                translateCraftsToPlanet(update.planet, update.displacement)
+            }
             MapUpdateManager.getInstance().scheduleUpdates(moonUpdateSet)
             return
         }
+        translateCraftsToPlanet(poll.planet, poll.displacement)
         MapUpdateManager.getInstance().scheduleUpdate(poll)
     }
 
+    private fun translateCraftsToPlanet(planet : Planet, displacement : ImmutableVector) {
+        val newCenter = planet.center.add(displacement)
+        val craftsToTeleport = HashSet<Craft>()
+        val center = MovecraftLocation(newCenter.x, newCenter.y, newCenter.z)
+        for (craft in CraftManager.getInstance().getCraftsInWorld(planet.space)) {
+            for (ml in craft.hitBox) {
+                if (ml.distance(center) > planet.radius)
+                    continue
+                craftsToTeleport.add(craft)
+                break
+            }
+        }
+        for (craft in craftsToTeleport) {
+            val hitBox = craft.hitBox
+            val midpoint = hitBox.midPoint
+            val y = planet.exitHeight - (hitBox.yLength / 2) - 5
+            var destLoc : MovecraftLocation? = null
+            while (destLoc == null) {
+                val bounds = ExpansionManager.worldBoundrary(planet.destination)
+                val minX = (bounds[0] + (hitBox.xLength / 2)) + 10
+                val maxX = (bounds[1] - (hitBox.xLength / 2)) - 10
+                val minZ = (bounds[2] + (hitBox.zLength / 2)) + 10
+                val maxZ = (bounds[3] - (hitBox.zLength / 2)) - 10
+                val x = Random.nextInt(minX, maxX)
+                val z = Random.nextInt(minZ, maxZ)
+                val test = MovecraftLocation(x, y, z)
+
+                if (!MathUtils.withinWorldBorder(planet.destination, test)) {
+                    continue
+                }
+
+                if (craft.notificationPlayer != null && !ExpansionManager.allowedArea(craft.notificationPlayer!!, test.toBukkit(planet.destination))) {
+                    continue
+                }
+                val diff = test.subtract(midpoint)
+                val chunks = ChunkManager.getChunks(craft.hitBox, planet.destination, diff.x, diff.y, diff.z)
+                MovecraftChunk.addSurroundingChunks(chunks, 3)
+                ChunkManager.syncLoadChunks(chunks)
+                val testType = test.toBukkit(planet.destination).block.type
+                if (!testType.name.endsWith("AIR") && !craft.type.passthroughBlocks.contains(testType)) {
+                    continue
+                }
+                val obstructed = Bukkit.getScheduler().callSyncMethod(MovecraftSpace.instance) {
+                    var obstructed = false;
+                    for (ml in craft.hitBox) {
+                        val destHitBoxLoc = ml.add(diff)
+                        val destHitBoxLocType = destHitBoxLoc.toBukkit(planet.destination).block.type
+                        if (planet.space.equals(planet.destination) && planet.contains(ml.toBukkit(planet.destination))) {
+                            obstructed = true
+                            break
+                        }
+                        if (!destHitBoxLocType.name.endsWith("AIR") && !craft.type.passthroughBlocks.contains(destHitBoxLocType)) {
+                            obstructed = true
+                            break
+                        }
+                        if (!MathUtils.withinWorldBorder(planet.destination, destHitBoxLoc)) {
+                            obstructed = true
+                            break
+                        }
+
+                    }
+                    obstructed
+                }.get()
+                if (obstructed)
+                    continue
+                destLoc = diff
+            }
+            craft.notificationPlayer!!.sendMessage("Entering " + planet.destination.name)
+            craft.translate(planet.destination, destLoc.x, destLoc.y, destLoc.z)
+        }
+    }
 }
